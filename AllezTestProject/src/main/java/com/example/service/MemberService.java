eb8a37d6a1ba436d9b449c8ad80955d95cc416fe
vegetablefried <loveeld132@naver.com>
2021-12-12 15:40:48 +0900

    -save------db내용가져오기---2021.12.12

---
+++ AllezTestProject/src/main/java/com/example/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.dto.MemberVO;
+ 
+public interface MemberService {
+    
+    public List<MemberVO> selectMember() throws Exception;
+}
+
