3b61966794804893f86757cf0215ff4e9091cff7
vegetablefried <63182566+vegetablefried@users.noreply.github.com>
2022-01-11 13:51:50 +0900

    Add files via upload

---
+++ AllezShop/src/main/java/com/example/dto/MemberVO.java
@@ -0,0 +1,65 @@
+package com.example.dto;
+
+public class MemberVO {
+	 
+    private String id;
+    private String pw;
+    private String name;
+    private String nickname;
+    private String address;
+    private String sex;
+    private String sign_day;
+    
+    public String getId() {
+        return id;
+    }
+ 
+    public void setId(String id) {
+        this.id = id;
+    }
+ 
+    public String getPw() {
+        return pw;
+    }
+ 
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getnickname() {
+        return nickname;
+    }
+ 
+    public void setnickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getaddress() {
+        return address;
+    }
+ 
+    public void setaddress(String address) {
+        this.address = address;
+    }
+    public String getsex() {
+        return sex;
+    }
+ 
+    public void setsex(String sex) {
+        this.sex = sex;
+    }
+    public String getsign_day() {
+        return sign_day;
+    }
+ 
+    public void setsign_day(String sign_day) {
+        this.sign_day = sign_day;
+    }
+}
+ 
